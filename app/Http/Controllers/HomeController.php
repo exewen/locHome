8c5d42ea110f1d756705601e914927c8f3d21d9b
exeweb <240175200@qq.com>
2019-12-14 13:07:39 +0800

    配置

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -12,49 +12,19 @@ class HomeController extends Controller {
         exit(var_dump($users));
     }
 
-    public function index() {
-        $configTab = [
+    private function getConfigOne() {
+        $tab = [
             'lan' => [
                 'type' => 'warehouse',
                 'name' => 'LAN',
                 'color' => '#00838F',
-                'domain' => 'http://localhost:8888/wms4sz2'
+                'domain' => 'http://127.0.0.1'
             ],
             'dev' => [
                 'type' => 'warehouse',
                 'name' => '开发',
                 'color' => '#00838F',
-                'domain' => 'http://wms4sz2.dev.yafex.cn'
-            ],
-            'd01' => [
-                'type' => 'warehouse',
-                'name' => '深圳仓',
-                'color' => '#F9A825',
-                'domain' => 'http://wms4sz1.yafex.cn'
-            ],
-            'd02' => [
-                'type' => 'warehouse',
-                'name' => '浒关仓',
-                'color' => '#558B2F',
-                'domain' => 'http://wms4sz2.yafex.cn:8040'
-            ],
-            'd03' => [
-                'type' => 'warehouse',
-                'name' => '苏州仓',
-                'color' => '#D84315',
-                'domain' => 'http://wms4sz3.yafex.cn:8050'
-            ],
-            'd04' => [
-                'type' => 'warehouse',
-                'name' => '菲律宾仓',
-                'color' => '#6D4C41',
-                'domain' => 'http://wms4ph1.yafex.cn'
-            ],
-            'd10' => [
-                'type' => 'warehouse',
-                'name' => '广州仓',
-                'color' => '#F9A825',
-                'domain' => 'http://wms4gz1.yafex.cn'
+                'domain' => 'http://qa-wms.dev.interfocus.org'
             ],
             'build' => [
                 'type' => 'build',
@@ -65,11 +35,11 @@ class HomeController extends Controller {
         ];
         $jsUrl = '';
         $mqUrl = '';
-        foreach ($configTab as $item) {
+        foreach ($tab as $item) {
             $item['type'] === 'warehouse' && $mqUrl .= $item['domain'] . '/?c=of_base_com_mq&__OF_DEBUG__=,';
             $item['type'] === 'warehouse' && $jsUrl .= $item['domain'] . '/?c=of_base_htmlTpl_tool&a=index&__OF_DEBUG__=,';
         }
-        $configTabUrl = [
+        $url = [
             'OA需求' => [
                 'url' => 'https://oa.yafex.cn/?c=gts_task&a=index&type=0&indexType=2&devUser=%E8%92%8B%E5%B0%9A%E5%90%9B',
                 'target' => '_self',
@@ -95,11 +65,6 @@ class HomeController extends Controller {
                 'target' => '_self',
                 'type' => 'build',
             ],
-//            'TAPD' => [
-//                'url' => 'https://www.tapd.cn/my_worktable',
-//                'target' => '_self',
-//                'type' => 'build',
-//            ],
             'APS' => [
                 'url' => 'http://localhost:8888/aps/src/',
                 'target' => '_self',
@@ -151,9 +116,71 @@ class HomeController extends Controller {
                 'type' => 'warehouse',
             ],
         ];
+        return [$tab, $url];
+    }
+
+    private function getConfigTwo() {
+        $tab = [
+            'lan' => [
+                'type' => 'warehouse',
+                'name' => 'LAN',
+                'color' => '#00838F',
+                'domain' => 'http://127.0.0.1'
+            ],
+            'dev' => [
+                'type' => 'warehouse',
+                'name' => '开发',
+                'color' => '#00838F',
+                'domain' => 'http://qa-wms.dev.interfocus.org'
+            ],
+            'build' => [
+                'type' => 'build',
+                'color' => '#6A1B9A',
+                'name' => '工具',
+                'domain' => ''
+            ],
+        ];
+        $jsUrl = '';
+        $mqUrl = '';
+        foreach ($tab as $item) {
+            $item['type'] === 'warehouse' && $mqUrl .= $item['domain'] . '/?c=of_base_com_mq&__OF_DEBUG__=,';
+            $item['type'] === 'warehouse' && $jsUrl .= $item['domain'] . '/?c=of_base_htmlTpl_tool&a=index&__OF_DEBUG__=,';
+        }
+        $url = [
+            '主页' => [
+                'url' => '/',
+                'target' => '_self',
+                'type' => 'warehouse',
+            ],
+            'MBB' => [
+                'url' => '/mobile.php',
+                'target' => '_self',
+                'type' => 'warehouse',
+            ],
+            '企业邮箱' => [
+                'url' => 'https://exmail.qq.com/login',
+                'target' => '_self',
+                'type' => 'build',
+            ],
+            '系统概述' => [
+                'url' => 'https://eat5vr.axshare.com/',
+                'target' => '_self',
+                'type' => 'build',
+            ],
+            'QA产品' => [
+                'url' => 'https://zaaay8.axshare.com/#id=bjd41p&p=%E6%9D%A1%E7%A0%81%E6%96%B9%E6%A1%88&g=1',
+                'target' => '_self',
+                'type' => 'build',
+            ],
+        ];
+        return [$tab, $url];
+    }
+
+    public function index() {
+        $urlArr = $this->getConfigTwo();
         return view('home.index', [
-            'configTab' => $configTab,
-            'configTabUrl' => $configTabUrl,
+            'configTab' => $urlArr[0],
+            'configTabUrl' => $urlArr[1],
         ]);
     }
 
